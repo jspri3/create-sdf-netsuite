5982897606cac3e212d8417f583dd2ab8440727a
Mayer Lench <mlench@finitydevelopment.com>
2020-11-26 17:15:22 -0500

    fixes some file naming issues

--- templates/sdf/src/FileCabinet/SuiteScripts/%-FILE_CAB_FOLDER-%/clients/%-PROJECT_INITIALS-%_%-PROJECT_ID.toLowerCase()-%_cs.ts
+++ templates/sdf/src/FileCabinet/SuiteScripts/%-FILE_CAB_FOLDER-%/clients/%-PROJECT_INITIALS-%_%-PROJECT_ID.toLowerCase()-%_cs.ts
@@ -9,39 +9,39 @@ export let fieldChanged: EntryPoints.Client.fieldChanged = (context: EntryPoints
 
 }
 
-export let lineInit: EntryPoints.Client.lineInit = (context: EntryPoints.Client.lineInitContext) => {
+// export let lineInit: EntryPoints.Client.lineInit = (context: EntryPoints.Client.lineInitContext) => {
 
-}
+// }
 
-export let pageInit: EntryPoints.Client.pageInit = (context: EntryPoints.Client.pageInitContext) => {
+// export let pageInit: EntryPoints.Client.pageInit = (context: EntryPoints.Client.pageInitContext) => {
 
-}
+// }
 
-export let postSourcing: EntryPoints.Client.postSourcing = (context: EntryPoints.Client.postSourcingContext) => {
+// export let postSourcing: EntryPoints.Client.postSourcing = (context: EntryPoints.Client.postSourcingContext) => {
 
-}
+// }
 
-export let saveRecord: EntryPoints.Client.saveRecord = (context: EntryPoints.Client.saveRecordContext) => {
+// export let saveRecord: EntryPoints.Client.saveRecord = (context: EntryPoints.Client.saveRecordContext) => {
 
-}
+// }
 
-export let sublistChanged: EntryPoints.Client.sublistChanged = (context: EntryPoints.Client.sublistChangedContext) => {
+// export let sublistChanged: EntryPoints.Client.sublistChanged = (context: EntryPoints.Client.sublistChangedContext) => {
 
-}
+// }
 
-export let validateDelete: EntryPoints.Client.validateDelete = (context: EntryPoints.Client.validateDeleteContext) => {
+// export let validateDelete: EntryPoints.Client.validateDelete = (context: EntryPoints.Client.validateDeleteContext) => {
 
-}
+// }
 
-export let validateField: EntryPoints.Client.validateField = (context: EntryPoints.Client.validateFieldContext) => {
+// export let validateField: EntryPoints.Client.validateField = (context: EntryPoints.Client.validateFieldContext) => {
 
-}
+// }
 
-export let validateInsert: EntryPoints.Client.validateInsert = (context: EntryPoints.Client.validateInsertContext) => {
+// export let validateInsert: EntryPoints.Client.validateInsert = (context: EntryPoints.Client.validateInsertContext) => {
 
-}
+// }
 
-export let validateLine: EntryPoints.Client.validateLine = (context: EntryPoints.Client.validateLineContext) => {
+// export let validateLine: EntryPoints.Client.validateLine = (context: EntryPoints.Client.validateLineContext) => {
 
-}
+// }
 
