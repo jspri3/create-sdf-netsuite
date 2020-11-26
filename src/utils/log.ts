a5ce2256e7751424a753ce4fd1ab7fe6e9facf25
Mayer Lench <mlench@finitydevelopment.com>
2020-11-26 17:29:15 -0500

    adds auto install of dependancies

--- src/utils/log.ts
+++ src/utils/log.ts
@@ -1,4 +1,5 @@
 import chalk from 'chalk'
+import * as shelljs from 'shelljs'
 import { CliOptions, CliAnswers } from '../types'
 
 export function doneMessage(o: CliOptions, a: CliAnswers) {
@@ -8,11 +9,15 @@ export function doneMessage(o: CliOptions, a: CliAnswers) {
 		chalk.magenta(`This project is backed by ${chalk.green('Finity Development')}, https://finitydevelopment.com`)
 	)
 	console.log('')
-	console.log(chalk.blue(`cd ${a.PROJECT_NAME}/sdf npm i`))
+	console.log(chalk.blue(`Installing node_modules with npm i`))
+	shelljs.exec(`cd ${a.PROJECT_NAME} && npm i`)
 	console.log('')
 	console.log(chalk.green('Done!'))
 	console.log(chalk.green(`Happy Hacking :)`))
-
+	console.log('')
+	console.log(
+		chalk.magentaBright(`FYI, did you know ${chalk.green('Finity Development')} has an amazing Netsuite team. Shoot us an email for your next project support@finitydevelopment.com`)
+	)
 	const message = o.config.postMessage
 
 	if (message) {
